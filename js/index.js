e9d0ae1fac8cad9340b0170fc0ee48a24ffadffd
Joaquin Arriola <joaquinarriola25@gmail.com>
2022-09-30 16:04:24 -0300

    se agregaron funciones

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
-usuario = prompt("Ingrese usuario");
-contraseña = prompt("Ingrese su contraseña");
-alert(`Bievenido, ${usuario}`)
+function saludar() {
+    alert("¡Hola formuleros!")
+}
+saludar()
 
 
+let nombre = prompt("Ingresar nombre")
+let apellido = prompt("Ingresar apellido")
+
+
+if((nombre !="") || (apellido !="")){
+    alert("Nombre: " +nombre +"\nApellido: " +apellido);
+}  else {
+    alert("Debes ingresar tu nombre y apellido")
+}
+
+
+
+function solicitarUsuario () {
+    let usuario = prompt("Ingresar usuario")
+    let contraseña = prompt("Ingresar contraseña")
+    if((usuario !="") || (contraseña !="")){
+    alert("Bievenido: " +usuario);
+}  else {
+    alert("Debes ingresar un usuario o contraseña valido")
+} 
+}
+solicitarUsuario()
